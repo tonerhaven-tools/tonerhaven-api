dabec5572fbe3714052338e53aab4809127331f4
Jesse Canonigo <strifejeyz@gmail.com>
2023-09-05 01:06:59 -0400

    Replaced the deleted application.js with the .env config

--- models/Auth.js
+++ models/Auth.js
@@ -1,5 +1,4 @@
 const jwt = require('jsonwebtoken')
-const Config   = require('../configs/application');
 
 const Auth = (req, res, next) => {
     const tokenCookie = req.headers.cookie?.split(';').map(cookie => cookie.trim()).find(cookie => cookie.startsWith('token='));
@@ -8,7 +7,7 @@ const Auth = (req, res, next) => {
         return res.sendStatus(401);
     }
     const token = tokenCookie.split('=')[1];
-    jwt.verify(token, Config.SECRET_KEY, (err, user) => {
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
         if (err) {
             console.log("Forbidden");
             return res.sendStatus(403);
