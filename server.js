9abf136f8baf973ebcceec6bb1743bbfec13baf6
Kim Avillanosa <avillanosakzxc@gmail.com>
2023-09-05 22:30:32 +0800

    modified static files

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
 const server_port = 3001;
 const brands_routes = require("./controllers/brands_controller");
 const products_routes = require("./controllers/products_controller");
-const serverless  = require("serverless-http");
+const serverless = require("serverless-http");
 
 //Swagger definitions
 const swaggerUi = require("swagger-ui-express");
@@ -16,14 +16,12 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-
 /*** ROUTES > Controllers ***/
-app.use("/api/brands",   brands_routes);
+app.use("/api/brands", brands_routes);
 app.use("/api/products", products_routes);
 
 // Serve files inside storage
-app.use('/api/storage',  express.static('./storage'));
-
+app.use("/api/storage", express.static(`${__dirname}/storage`));
 
 /** Node Server Log **/
 app.listen(server_port, () => {
