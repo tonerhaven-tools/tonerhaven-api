7c76c3e1373e50e28ee36feff476bde418f6d828
Jesse Canonigo <strifejeyz@gmail.com>
2023-09-05 01:11:58 -0400

    - Added cors (to support webapp proxy) - Removed some user routes - Added line #25 to serve the product images - Added routes for brands, products

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 
 const server_port = 3001;
-const user_routes = require("./controllers/user_controller");
-
-//Environment Variables
-const serverless = require("serverless-http");
+const brands_routes = require("./controllers/brands_controller");
+const products_routes = require("./controllers/products_controller");
+const serverless  = require("serverless-http");
 
 //Swagger definitions
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-//Controllers / routes
-app.use("/api/users", user_routes);
 
-/*** ROUTES ***/
+/*** ROUTES > Controllers ***/
+app.use("/api/brands",   brands_routes);
+app.use("/api/products", products_routes);
 
-app.get("/api", (request, response) => {
-  response.json("You've reached Express backend.");
-});
+// Serve files inside storage
+app.use('/api/storage',  express.static('./storage'));
+
 
 /** Node Server Log **/
 app.listen(server_port, () => {
-  console.log(`App started`);
+  console.log(`App started on port ${server_port}`);
 });
 
 module.exports.handler = serverless(app);
