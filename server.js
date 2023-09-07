9d9c3e25763bb39c574b632e29731e6e748fd0c7
Jesse Canonigo <strifejeyz@gmail.com>
2023-09-07 02:42:31 -0400

    Modified cors, added new routes

--- server.js
+++ server.js
@@ -1,35 +1,43 @@
-const express = require("express");
 const cors = require("cors");
+const express = require("express");
 const serverless = require("serverless-http");
-
-//
-const app = express();
-var path = require("path");
-const server_port = 3001;
-const brands_routes = require("./controllers/brands_controller");
-const products_routes = require("./controllers/products_controller");
-const profile_routes = require("./controllers/profiles_controller");
-const acct_checks_routes = require("./controllers/acctchecks_controller");
 
 //Swagger definitions
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
-app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+const app = express();
+const server_port = 3001;
+const brands_routes      = require("./controllers/brands_controller");
+const products_routes    = require("./controllers/products_controller");
+const profile_routes     = require("./controllers/profiles_controller");
+const acct_checks_routes = require("./controllers/acctchecks_controller");
+const dashboard_routes   = require("./controllers/dashboard_controller");
 
-/*** ROUTES > Controllers ***/
-app.use("/api/brands", brands_routes);
+const corsOptions = {
+  origin: "https://localhost:5173",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+// ROUTES > Controllers
+app.use("/api/brands",   brands_routes);
 app.use("/api/products", products_routes);
 app.use("/api/profiles", profile_routes);
-app.use("/api/check", acct_checks_routes);
+app.use("/api/check",    acct_checks_routes);
+app.use('/api/dashboard',dashboard_routes);
 
 // Serve files inside storage
-app.use("/api/storage", express.static(path.join(__dirname, "storage")));
+app.use("/storage", express.static(require("path").join(__dirname, "storage")));
 
-/** Node Server Log **/
+// Node Server Log
 app.listen(server_port, () => {
   console.log(`App started on port ${server_port}`);
 });
