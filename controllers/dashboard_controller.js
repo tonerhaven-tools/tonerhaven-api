922c840fd2d43e2dc1949dc4432af7685216df50
Jesse Canonigo <strifejeyz@gmail.com>
2023-09-08 01:12:36 -0400

    Added insert and delete operations

--- controllers/dashboard_controller.js
+++ controllers/dashboard_controller.js
@@ -1,9 +1,32 @@
-const express = require("express");
-const router = express.Router();
-const BrandsModel = require("../models/brands_model");
-const ProductsModel = require("../models/products_model");
+const express         = require("express");
+const router          = express.Router();
+const multer          = require('multer');
+const BrandsModel     = require("../models/brands_model");
+const ProductsModel   = require("../models/products_model");
 const CategoriesModel = require("../models/categories_model");
 
+
+// Upload the file to storage
+const storage = multer.diskStorage({
+  destination: (req,file,cb) => cb(null,'storage/uploads/products/'),
+  filename:    (req,file,cb) => cb(null,file.fieldname+'-'+Date.now()+require('path').extname(file.originalname))
+});
+const upload = multer({ storage: storage });
+
+// Store a new Product
+router.post("/store-product", upload.single('thumbnail'), async(req,res) => {
+  const insert = await (new ProductsModel(req, res)).store_product(req.file.filename);
+  return res.json(insert);
+});
+
+// Delete a Product
+router.post("/destroy-product", async(req,res) => {
+  const destroy = await (new ProductsModel(req, res)).destroy_product();
+  return res.json(destroy);
+});
+
+
+// Fetch product information for Dashboard
 router.get("/product-related-data", async (req, res) => {
   const all_brands = await (new BrandsModel(req,res)).all_brands();
   const categories = await (new CategoriesModel(req,res)).all_categories();
@@ -12,10 +35,6 @@ router.get("/product-related-data", async (req, res) => {
   return res.json({
     all_brands, products, categories
   });
-});
-
-router.post("/store-product", async (req, res) => {
-  res.json(req.body); // Why is this shit is empty
 });
 
 module.exports = router;
--- models/products_model.js
+++ models/products_model.js
@@ -13,5 +13,34 @@ class ProductsModel extends ModelBase {
       throw new Error("Internal server error");
     }
   }
+
+  async store_product(filename) {
+    try {
+      const form_values = this.request.body;
+      form_values.thumbnail = filename;
+      return await this.table.insert(form_values).returning('*');
+    } catch (error) {
+      console.error(error);
+      throw new Error("Internal server error");
+    }
+  }
+
+  async destroy_product() {
+    try {
+      const prod_id  = await this.request.body.product_id;
+      const product  = await this.table.where('id',prod_id).first();
+      const filesys  = require('fs').promises;
+      const filepath = `storage/uploads/products/${product.thumbnail}`;
+
+      return {
+        image_deleted: await filesys.unlink(filepath),
+        product_delete: await this.table.where('id',prod_id).delete(),
+      }
+    }
+    catch (error) {
+      console.error(error);
+      throw new Error("Internal server error");
+    }
+  }
 }
 module.exports = ProductsModel;
