0cb307e1493874e5eff787fd21a73bce81ffc578
Kim Avillanosa <avillanosakzxc@gmail.com>
2023-09-10 13:50:45 +0800

    modified paths for swagger

--- server.js
+++ server.js
@@ -4,17 +4,19 @@ const serverless = require("serverless-http");
 
 //Swagger definitions
 const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger.json");
+const swaggerDocument = require("./swaggerdoc/swagger.json");
 
 const app = express();
 const server_port = 3001;
+const server_host = "127.0.0.1";
+
 const brands_routes = require("./controllers/brands_controller");
 const products_routes = require("./controllers/products_controller");
 const profile_routes = require("./controllers/profiles_controller");
 const acct_checks_routes = require("./controllers/acctchecks_controller");
 const dashboard_routes = require("./controllers/dashboard_controller");
 
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "*", methods: "GET,HEAD,PUT,PATCH,POST,DELETE" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -30,8 +32,14 @@ app.use("/api/dashboard", dashboard_routes);
 app.use("/storage", express.static(require("path").join(__dirname, "storage")));
 
 // Node Server Log
-app.listen(server_port, () => {
-  console.log(`App started on port ${server_port}`);
+
+const server = app.listen(server_port, server_host, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  const baseUrl = `http://${host}:${port}`;
+  const swagger = `http://${host}:${port}/swagger`;
+  console.log(`Server is listening at ${baseUrl}`);
+  console.log(`API Docs at ${swagger}`);
 });
 
 module.exports.handler = serverless(app);
---
+++ swagger.js
@@ -0,0 +1,17 @@
+const swaggerAutogen = require("swagger-autogen")();
+
+const doc = {
+  info: {
+    title: "Toner Haven API",
+    description: "Web service for tonerhaven api",
+  },
+  host: "localhost:3001",
+  schemes: ["https", "http"],
+};
+
+const outputFile = "./swaggerdoc/swagger.json";
+const endpointsFiles = [`./server.js`];
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+  require("./server.js"); // Your project's root file
+});
